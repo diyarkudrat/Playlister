2d2015ee68710585180b28d411cf3e3287208b18
Diyar Kudrat <dkudrat@gmail.com>
2019-10-01 17:14:43 -0700

    dealt with Heroku error

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os
 
 client = MongoClient()
 db = client.Playlister
+playlists = db.playlists
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=host)
+db = client.get_default_database()
 playlists = db.playlists
 
 
